1f7b61548819a55acbb9aa171b9a44c62e4fb6ee
anoop007 <anoopviswanadh007@gmail.com>
2024-06-09 16:24:39 +0530

    feature: current user info

--- src/typeRefs/customer/quries.ts
+++ src/typeRefs/customer/quries.ts
@@ -2,6 +2,7 @@ import { gql } from "apollo-server-express";
 
 export const UserQuries = gql`
   type Query {
+    me: User
     user(_id: ID, email: String): User
     verifyInvitation(token: String): User
     token(tokenRequestInput:TokenRequestInput): Token
