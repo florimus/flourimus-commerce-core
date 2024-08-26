d21d8c195487a9022dc3972179dc2126bfe5be6f
anoopss <anoopviswanadh007@gmail.com>
2024-08-27 01:26:44 +0530

    feature: adding category input validations

--- src/service/impl/categoryServiceImpl.ts
+++ src/service/impl/categoryServiceImpl.ts
@@ -1,7 +1,26 @@
 import sequence from "@core/sequence";
 import { CategoryCreateMutationArgsType, ContextObjectType } from "@core/types";
 import { getCurrentTime } from "@core/utils/timeUtils";
+import BadRequestError from "@errors/BadrequestError";
+import NotFoundError from "@errors/NotFoundError";
 import categoryRepository from "@repositories/categoryRepository";
+
+/**
+ * Controller used to get category info
+ * @param categoryCreateInput
+ * @param context
+ * @returns
+ */
+const getCategoryById = async (id: string, isActive: boolean = false) => {
+  if (!id) {
+    throw new BadRequestError("Id is mandatory");
+  }
+  const category = await categoryRepository.findcategoryById(id, isActive);
+  if (category?._id) {
+    return category;
+  }
+  throw new NotFoundError("Category not found");
+};
 
 /**
  * Controller used to create category
@@ -9,10 +28,34 @@ import categoryRepository from "@repositories/categoryRepository";
  * @param context
  * @returns
  */
-export const createcategory = async (
+const createcategory = async (
   categoryCreateInput: CategoryCreateMutationArgsType["categoryCreateInput"],
   context: ContextObjectType
 ) => {
+  const { name, productIds, subCategoryIds, parentId } =
+    categoryCreateInput || {};
+  if (!name) {
+    throw new BadRequestError("Name is mandatory");
+  }
+  if (!name) {
+    throw new BadRequestError("Name is mandatory");
+  }
+  if (parentId) {
+    await getCategoryById(parentId);
+  }
+  if (subCategoryIds) {
+    if (subCategoryIds.includes(parentId)) {
+      throw new BadRequestError("Parent category cannot be its sub category");
+    }
+    const isAllCategoriesExits =
+      await categoryRepository.checkAllCategoriesExits(subCategoryIds);
+    if (!isAllCategoriesExits) {
+      throw new BadRequestError("Subcategories not found");
+    }
+  }
+  if (productIds) {
+    // TODO: need to verify product ids
+  }
   const categoryId = await sequence.categoryId();
   const category = await categoryRepository.createCategory({
     ...categoryCreateInput,
@@ -29,4 +72,5 @@ export const createcategory = async (
 
 export default {
   createcategory,
+  getCategoryById,
 };
